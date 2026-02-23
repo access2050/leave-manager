1e05e383e1eb8ed18b44de3cf2742cc96ab3f50b
access2050 <access2050@empal.com>
2026-02-23 17:47:39 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "leave-pwa-v8-1";
+const CACHE_NAME = "leave-pwa-v9-combo";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
