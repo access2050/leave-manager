38c6a19859d568c25c6ffaba179d272af5bb2098
access2050 <access2050@empal.com>
2026-08-22 13:41:41 +0900

    service-worker.js 업데이트

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "leave-pwa-v9-20260822-1329";
+const CACHE_NAME = "leave-pwa-v9-20260822-1341";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
